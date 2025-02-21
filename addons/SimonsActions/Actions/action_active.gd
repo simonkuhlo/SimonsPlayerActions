5b3883d48e03a91122740ba7abe843e614b63768
Simon <siga241@gmail.com>
2025-02-21 17:17:09 +0100

    Fix: Return to ready state if preparing of activeskill gets interrupted

--- addons/SimonsActions/Actions/action_active.gd
+++ addons/SimonsActions/Actions/action_active.gd
@@ -11,3 +11,5 @@ func _on_ready_state_processing(delta):
 func _on_preparing_state_processing(delta):
 	if Input.is_action_pressed(trigger):
 		super._on_preparing_state_processing(delta)
+	else:
+		state.travel(state.ready_state)
